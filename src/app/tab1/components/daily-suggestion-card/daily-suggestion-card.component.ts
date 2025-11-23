9304852b72800e99501b0aaeb9feeb0275191060
julian <julian@MacBook-Air-de-julian.local>
2025-11-23 13:26:44 -0500

    fix: add new components for tabs and just styling app

--- src/app/tab1/components/daily-suggestion-card/daily-suggestion-card.component.ts
+++ src/app/tab1/components/daily-suggestion-card/daily-suggestion-card.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { chevronForwardOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-daily-suggestion-card',
   templateUrl: './daily-suggestion-card.component.html',
   styleUrls: ['./daily-suggestion-card.component.scss'],
+  imports: [IonIcon]
 })
 export class DailySuggestionCardComponent  implements OnInit {
 
-  constructor() { }
+  constructor() {
+    addIcons({ chevronForwardOutline });
+  }
 
   ngOnInit() {}
 
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
-import {IonContent } from '@ionic/angular/standalone';
+import { IonContent } from '@ionic/angular/standalone';
 import { DashboardHeaderComponent } from './components/dashboard-header/dashboard-header.component';
 import { NgondroProgressCardComponent } from './components/ngondro-progress-card/ngondro-progress-card.component';
 import { ResumeSessionCardComponent } from './components/resume-session-card/resume-session-card.component';
+import { DailySuggestionCardComponent } from './components/daily-suggestion-card/daily-suggestion-card.component';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonContent, DashboardHeaderComponent, NgondroProgressCardComponent, ResumeSessionCardComponent],
+  imports: [IonContent, DashboardHeaderComponent, NgondroProgressCardComponent, ResumeSessionCardComponent, DailySuggestionCardComponent],
 })
 export class Tab1Page {
-  constructor() {}
+  constructor() { }
 }
