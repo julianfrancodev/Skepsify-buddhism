f5db0f6c6cb6e0d1969d41160b40f0baf0bc43f9
julian <julian@MacBook-Air-de-julian.local>
2025-11-23 09:27:06 -0500

    fix: add new components for tabs and just styling app

--- src/app/tab1/components/resume-session-card/resume-session-card.component.ts
+++ src/app/tab1/components/resume-session-card/resume-session-card.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { playOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-resume-session-card',
   templateUrl: './resume-session-card.component.html',
   styleUrls: ['./resume-session-card.component.scss'],
+  imports: [IonIcon]
 })
 export class ResumeSessionCardComponent  implements OnInit {
 
-  constructor() { }
+  constructor() {
+        addIcons({ playOutline });
+
+   }
 
   ngOnInit() {}
 
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import {IonContent } from '@ionic/angular/standalone';
 import { DashboardHeaderComponent } from './components/dashboard-header/dashboard-header.component';
 import { NgondroProgressCardComponent } from './components/ngondro-progress-card/ngondro-progress-card.component';
+import { ResumeSessionCardComponent } from './components/resume-session-card/resume-session-card.component';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonContent, DashboardHeaderComponent, NgondroProgressCardComponent],
+  imports: [IonContent, DashboardHeaderComponent, NgondroProgressCardComponent, ResumeSessionCardComponent],
 })
 export class Tab1Page {
   constructor() {}
